52202237f47fd3f224a9060279e7f46616bb6218
Rahulpoppad <x23235535@student.ncirl.ie>
2024-04-22 14:09:34 +0300

    Commit

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Poppad Rahul',                   # Type in your name
   author_email = 'rahulpoppad.1999@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Rahulpoppad/django-eventify',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Rahulpoppad/django-eventify/archive/refs/tags/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Rahulpoppad/django-eventify/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['EVENT', 'BOOKING'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'django',
